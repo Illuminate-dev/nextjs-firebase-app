897427b7c52cf2bf18b46c7199b57f16665f8a6b
henry <henrydbeveridge@gmail.com>
2022-01-08 17:25:04 -0800

    delete buttoon

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -1,6 +1,8 @@
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { deleteDoc, doc } from "firebase/firestore";
 import Link from "next/link";
+import { auth, firestore } from "../lib/firebase";
 
 export default function PostFeed({ posts, admin }) {
     return posts ? posts.map((post) => <PostItem post={post} key={post.slug} admin={admin}/>) : null;
@@ -9,6 +11,14 @@ export default function PostFeed({ posts, admin }) {
 function PostItem({ post, admin = false}) {
     const wordCount = post?.content.trim().split(/\s+/g).length;
     const minutesToRead = (wordCount / 100 + 1).toFixed(0);
+
+
+    const deletePost = async () => {
+        const slug = post.slug;
+        
+        const ref = doc(firestore, 'users', auth.currentUser.uid, 'posts', slug);
+        await deleteDoc(ref);
+    }
 
     return <div className="card">
         <Link href={`/${post.username}`}>
@@ -25,12 +35,16 @@ function PostItem({ post, admin = false}) {
 
         {admin && (
         <>
-          <Link href={`/admin/${post.slug}`}>
-            <h3>
-              <button className="btn-blue">Edit</button>
-            </h3>
-          </Link>
-
+          <div>
+              <Link href={`/admin/${post.slug}`}>
+                <h3>
+                  <button className="btn-blue">Edit</button>
+                </h3>
+              </Link>
+              <h3>
+                  <button onClick={deletePost} className="btn-red">Delete Post</button>
+              </h3>
+          </div>
           {post.published ? <p className="text-success">Live</p> : <p className="text-danger">Unpublished</p>}
         </>
       )}
